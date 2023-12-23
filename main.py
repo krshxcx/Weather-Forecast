24edc5c4bf3b71dfafca6c9aa90a99c2ff872a6c
Sai Krishna Durgam <krishmusic.14@gmail.com>
2023-12-23 16:47:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import plotly.express as px
 from backend import get_data
 
 # Add title, slider, option box, etc.
-st.title("Weather Forecast for the Next Days:")
+st.title("Shonamma's Weather Forecast")
+
 place = st.text_input("Place:")
 days = st.slider("Forecast days", min_value=1, max_value=5)
 option = st.selectbox("Select data to view", ('Temperature', 'Sky'))
